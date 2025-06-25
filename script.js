808b41fd0695fbde2f68d8002e25db1305ea7385
LarissaSPinheiro <larissasteffen97@gmail.com>
2025-06-25 12:05:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const botao = document.querySelector("#exibirmensagem");
+const mensagem = document.getElementById("mensagem");
+
+// ocula a mensagem inicialmente via JS
+mensagem.style.display = "none";
+
+// Ao clicar, alterna entre mostrar e ocultar
+botao.addEventListener("click", mostrarmensagem);
+
+// Função exibe ou esconde a mensagem
+function mostrarmensagem() {
+    //verifica se a mensagem está oculta
+    if (mensagem.style.display === "none") 
+        {
+        //exibe a mensagem se oculta
+        mensagem.style.display = "block";
+        } 
+    //oculta quando estiver sendo exibida
+    else {
+        mensagem.style.display = "none";
+    }
+}
